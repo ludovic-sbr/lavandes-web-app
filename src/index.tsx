7ce1da66c1023590065e461c782995d80ad8be59
Ludovic Sbr <sobrero.ludovic@gmail.com>
2023-03-27 16:46:52 +0000

    feat : adding google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactGA from "react-ga4";
 import App from './app/App'
 import './index.css'
 
@@ -9,10 +10,19 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
+ReactGA.initialize("G-XJBFM94DLD");
+
 root.render(
   <Provider store={store}>
     <App/>
   </Provider>,
 );
 
-reportWebVitals();
+const SendAnalytics = ()=> {
+    ReactGA.send({
+        hitType: "pageview",
+        page: window.location.pathname,
+    });
+}
+
+reportWebVitals(SendAnalytics);
